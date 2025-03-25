2aba13ae864b725a741dd8f2ae35b14f00530a7a
rifqisaleh <mrifqisaleh@gmail.com>
2025-03-25 11:50:31 +0700

    docker connection estbalished

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.database import models  # Ensure models are imported
+from app.model.models import models # Ensure models are imported
 
 with app.app_context():
     db.create_all()
